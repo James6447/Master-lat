83920e293e933db3cb40afcbf4555632275215c6
James <jamesvyean38@gmail.com>
2018-05-09 13:45:49 +0800

    hamburger bar

--- lat/js/slider.js
+++ lat/js/slider.js
@@ -195,4 +195,54 @@ $('li').click(function() {
 // or outside the document ready, enter code here
 
   //YOUR JQUERY CODE
+
+
+//hamburger
+
+if($(window).width() > 800){
+    $("#test").show();
+    $(".icon").hide();
+ }
+else if ($(window).width() < 800){
+    $("#test").hide();
+    $(".icon").show();
+    $("#icon__open").show();
+    $("#icon__close").hide();
+ }
+
+  $("#icon__open").click(function() {
+    $("#test").show();
+    $("#icon__open").hide();
+    $("#icon__close").show();
+  });
+  $("#icon__close").click(function() {
+    $("#test").hide();
+    $("#icon__open").show();
+    $("#icon__close").hide();
+  });
+
+  $(window).resize(function(){
+    if($(window).width() > 800){
+        $("#test").show();
+        $(".icon").hide();
+     }
+    else if ($(window).width() < 800){
+        $("#test").hide();
+        $(".icon").show();
+        $("#icon__open").show();
+        $("#icon__close").hide();
+     }
+  });
+
+  (function() {
+    $('.hamburger').on('click', function(e) {
+      e.preventDefault();
+      $('.hamburger-line').toggleClass('on');
+      return $('.hamburger-bar').toggleClass('on');
+    });
+
+  }).call(this);
+
+
+
 }
