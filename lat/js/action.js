98dc1f302753c7a2886b57b4504018a535035132
zhijian090195 <zhijian090195@hotmail.com>
2018-07-24 00:22:36 +0800

    new

--- lat/js/action.js
+++ lat/js/action.js
@@ -41,7 +41,8 @@ function datacheck()
 function getData(theURL){
   $.get(theURL,
     function($status){
-      bootbox.alert("感謝您的留言，您的建議是我們前進的動力!");
+      alert("感謝您的留言，您的建議是我們前進的動力!");
+      location.href = "index.html";
     });
 }
 
