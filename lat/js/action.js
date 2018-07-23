1253b7a196026d989ad896d79e905f05e1a6ed92
James <jamesvyean38@gmail.com>
2018-07-23 23:41:00 +0800

    recover mailer

--- lat/js/action.js
+++ lat/js/action.js
@@ -41,7 +41,7 @@ function datacheck()
 function getData(theURL){
   $.get(theURL,
     function($status){
-      alert("感謝您的留言，您的建議是我們前進的動力!");
+      bootbox.alert("感謝您的留言，您的建議是我們前進的動力!");
       location.href = "index.html";
     });
 }
